3b9b50b9676567bbb369d404be4e4a4bb6519fba
MariaMsu <i.knaz@yandex.ru>
2023-03-23 21:47:16 +0100

    add more data

--- README.md
+++ README.md
@@ -5,14 +5,14 @@ The function takes as input a dataframe with the following columns:
 `event_id` – identifier of an event that happened inside an IDE.  
 Each event corresponds to action either of a user or an IDE itself. 
 For the sake of simplicity we assume that an event is a user action if event_id in (‘a’, ‘b’, ‘c’);
-`timestamp`;  
+`timestamp` in "yyyy-MM-dd HH:mm:ss" format;  
 `product_code` – shortened name of an IDE;
 
 The function identify user session based on events timestamps, user_ids and product_ids
 and assign the same user_session_ids to the table's rows describing the same user session.
 The user_session_ids have format user_id#product_code#timestamp.
 
-### How to create environment and run
+## How to create environment and run
 ```shell
 apt-get install python3.9-dev python3.9-venv
 python3.9 -m venv venv
@@ -23,10 +23,13 @@ pip3 install -r requirements.txt
 python3 main.py  # run the spark function itself
 ```
 
-Policy 1:
+## User session definition
+
+**Policy 1: time bounded actions (tb).**  
 A user session is a set of actions performed by a user or IDE
 with a short time interval between these actions.
 
-Policy 2:
+**Policy 2: actions between start and close (sc).**
 A user session is a set of all events for a distinct user happened 
-between the events 'ide.start' and 'ide.close'.
+between the events 'ide.start' and 'ide.close'. 
+If an ide was opened but was not yet closed, this set of action is also considered as a session.
--- main.py
+++ main.py
@@ -10,15 +10,15 @@ compute user_session_id based on events timestamps, user_ids and product_ids
 """
 
 
-def compute_session_id(df, session_time_threshold, debug=False):
+def compute_session_id_time_bound(df, session_time_threshold, debug=False):
     # Convert timestamp column to unix time
     df = df.withColumn("timestamp_long", F.unix_timestamp("timestamp", "yyyy-MM-dd HH:mm:ss").cast("long"))
 
     # Define a window specification based on the user_id and product_code columns, ordered by the timestamp_long column:
-    window_spec = Window.partitionBy("user_id", "product_code").orderBy("timestamp_long")
+    w_user_product = Window.partitionBy("user_id", "product_code").orderBy("timestamp_long")
 
     # Compute the time difference between consecutive events within each window and create a new column
-    df = df.withColumn("prev_timestamp", F.lag("timestamp_long", 1).over(window_spec))
+    df = df.withColumn("prev_timestamp", F.lag("timestamp_long", 1).over(w_user_product))
     df = df.withColumn("time_diff",
                        F.when(df.prev_timestamp.isNull(), None).otherwise(df.timestamp_long - df.prev_timestamp))
 
@@ -32,7 +32,7 @@ def compute_session_id(df, session_time_threshold, debug=False):
 
     # fill the empty event strings with the value from the closest previous row containing an event name
     df = df.withColumn('session_id', F.when(df['session_id'].isNull(), F.last('session_id', True)
-                                            .over(window_spec)).otherwise(df['session_id']))
+                                            .over(w_user_product)).otherwise(df['session_id']))
 
     if debug:
         # Drop the intermediate columns
@@ -41,19 +41,19 @@ def compute_session_id(df, session_time_threshold, debug=False):
     return df
 
 
-def compute_session_id2(df, debug=False):
+def compute_session_id_start_close(df, debug=False):
     # Convert timestamp column to unix time
     df = df.withColumn("timestamp_long", F.unix_timestamp("timestamp", "yyyy-MM-dd HH:mm:ss").cast("long"))
 
     # Define a window specification based on the user_id and product_code columns, ordered by the timestamp_long column:
-    window_spec_incr = Window.partitionBy("user_id", "product_code").orderBy("timestamp_long")
+    w_user_product = Window.partitionBy("user_id", "product_code").orderBy("timestamp_long")
     # fill session_id column for lines, representing new session
     df = df.withColumn("session_id", F.when(df.event_id != "ide.start", None).otherwise(
         F.concat_ws("#", "user_id", "product_code", "timestamp")
     ))
     # fill the empty event strings with the value from the closest previous row containing an event name
     df = df.withColumn('session_id', F.when(df['session_id'].isNull(), F.last('session_id', True)
-                                                 .over(window_spec_incr)).otherwise(df['session_id']))
+                                            .over(w_user_product)).otherwise(df['session_id']))
 
     w1 = Window.partitionBy("session_id").orderBy("timestamp_long")
     df = df.withColumn('after_close', F.when(F.lag(df.event_id, 1).over(w1) == "ide.close", 1))
@@ -84,13 +84,13 @@ if __name__ == "__main__":
     # Load the data into a Spark DataFrame, assuming the data is in a CSV file with headers:
     print(f'    Read date from {config.input_path}')
     df_initial = spark.read.format("csv").option("header", "true").load(config.input_path)
-    df_with_ids1 = compute_session_id(
+    df_with_ids1 = compute_session_id_time_bound(
         df=df_initial,
         session_time_threshold=config.session_time_threshold,
         debug=False)
     write_table(df_with_ids1, output_path=config.output_path1)
 
-    df_with_ids2 = compute_session_id2(
+    df_with_ids2 = compute_session_id_start_close(
         df=df_initial,
         debug=False)
     write_table(df_with_ids2, output_path=config.output_path2)
